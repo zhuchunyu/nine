d6e6adbd1d8174f4f7ae750cb4a98a5717343c60
Joker <zhuchunyu_14a@163.com>
2016-08-31 12:28:27 +0800

    test

--- app/actors/HelloActor.java
+++ app/actors/HelloActor.java
@@ -2,12 +2,16 @@ package actors;
 
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.avaje.ebean.Ebean;
+import models.Blog;
 
 public class HelloActor extends UntypedActor {
 
     public static Props props = Props.create(HelloActor.class);
 
     public void onReceive(Object message) throws Exception {
+        Blog blog = Ebean.find(Blog.class, 1);
+        System.out.println(blog);
         sender().tell("actor:"+message, self());
     }
 }
